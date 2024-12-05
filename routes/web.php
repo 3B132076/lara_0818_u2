4ab96f7e4edec79782f62b9c77b1b1f5b822197a
steshun <steven921107@yahoo.com.tw>
2024-12-05 23:20:43 +0800

    練習3 傳遞資料至 view 顯示

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HelloController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('home', [HomeController::class, 'index']);
+
+Route::get('hello/{name?}', [HelloController::class, 'index'])->name('hello.index');
 
